ee6cb97b843e8c6db2100726a296bcf6cd9a06f3
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-03-14 10:13:12 +0900

    AuditingFields.java 설명 추가

--- src/main/java/com/fastcampus/projectboardadmin/domain/AuditingFields.java
+++ src/main/java/com/fastcampus/projectboardadmin/domain/AuditingFields.java
@@ -17,13 +17,16 @@ import java.time.LocalDateTime;
 @Getter
 @ToString
 @EntityListeners(AuditingEntityListener.class)
+// 엔티티의 생성, 수정, 삭제 등의 이벤트를 감지하여 특정 동작을 수행
 @MappedSuperclass
+// 엔티티 별로 공통 필드가 존재하는 경우 불 필요한 중복 코드를 제거하기 위해 사용된다.
 public abstract class AuditingFields {
 
     /** 생성일시 */
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
+ 	// 데이터베이스에서 NULL 값을 허용하지 않는다.
     protected LocalDateTime createdAt;
 
     /** 생성자 */
