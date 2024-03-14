74b884fc9701a0911be016d5846afb2e11e4f02f
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-03-14 13:32:44 +0900

    AdminAccountController.java 설명추가

--- src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
@@ -25,11 +25,15 @@ public class AdminAccountController {
     }
 
     @ResponseBody
+	// 메서드가 반환하는 객체를 HTTP 응답의 본문(body)으로 사용함을 나타낸다.
     @GetMapping("/api/admin/members")
     public List<AdminAccountResponse> getMembers() {
         return adminAccountService.users().stream()
+		// adminAccountService를 사용하여 관리자 계정 정보를 가져온다.
                 .map(AdminAccountResponse::from)
+				// 가져온 각 관리자 계정 정보를 AdminAccountResponse 객체로 변환
                 .toList();
+				// 변환된 객체들을 리스트로 수집하여 반환
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT) // httpstatus를 특정한 곳으로 지정하는 것
