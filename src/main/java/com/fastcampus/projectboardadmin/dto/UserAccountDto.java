2d04e93974e7d809cf5ebe093cdc5e3c30b6a086
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-03-14 10:50:53 +0900

    UserAccountDto.java 설명추가

--- src/main/java/com/fastcampus/projectboardadmin/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/projectboardadmin/dto/UserAccountDto.java
@@ -1,4 +1,4 @@
-package com.fastcampus.projectboardadmin.dto;
+ㅇpackage com.fastcampus.projectboardadmin.dto;
 
 import java.time.LocalDateTime;
 
@@ -20,5 +20,8 @@ public record UserAccountDto(
     public static UserAccountDto of(String userId, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new UserAccountDto(userId, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
+
+	// 사용자는 필요에 따라 간편하게 UserAccountDto 객체를 생성할 수 있습니다.
+	// 필요한 정보만을 인자로 전달하여 객체를 생성할 수 있고, 나머지는 필요에 따라 넘겨줄 수 있습니다.
 
 }
