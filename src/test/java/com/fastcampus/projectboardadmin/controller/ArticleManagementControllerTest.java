f53005a9f86109b097c7054aa46735d1588d603c
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-03-14 14:09:15 +0900

    ArticleManagementControllerTest.java 설명 추가

--- src/test/java/com/fastcampus/projectboardadmin/controller/ArticleManagementControllerTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/controller/ArticleManagementControllerTest.java
@@ -22,6 +22,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @DisplayName("컨트롤러 - 게시글 관리")
 @Import({TestSecurityConfig.class, GlobalControllerConfig.class})
 @WebMvcTest(ArticleManagementController.class)
@@ -33,18 +34,23 @@ class ArticleManagementControllerTest {
     }
 
     @WithMockUser(username = "tester", roles = "USER")
+	// 이 어노테이션은 현재 테스트에서 사용될 사용자를 지정
     @DisplayName("[view][GET] 게시글 관리 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingArticleManagementView_thenReturnsArticleManagementView() throws Exception {
         // Given
         given(articleManagementService.getArticles()).willReturn(List.of());
+		// 'articleManagementService'의 'getArticles()' 메서드가 호출 될 때 반환할 값을 지정, 여기서는 빈 리스트를 반환하도록 지정
+
         // When & Then
         mvc.perform(get("/management/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("management/articles"))
                 .andExpect(model().attribute("articles", List.of()));
+				// 응답으로 받은 모델에 "articles"라는 속성이 있고, 그 값이 빈 리스트인지 확인합니다.
         then(articleManagementService).should().getArticles();
+		// 메서드가 호출되었는지를 확인
     }
 
     @WithMockUser(username = "tester", roles = "USER")
@@ -53,16 +59,22 @@ class ArticleManagementControllerTest {
     void givenArticleId_whenRequestingArticle_thenReturnsArticle() throws Exception {
         // Given
         Long articleId = 1L;
+		// 1번 게시글
         ArticleDto articleDto = createArticleDto("title", "content");
+		// 테스트에 사용될 가짜 게시글 데이터를 생성
         given(articleManagementService.getArticle(articleId)).willReturn(articleDto);
+		// 'articleManagementService'의 'getArticle()' 메서드가 호출될 때 어떤 값을 반환할지 지정
+
         // When & Then
         mvc.perform(get("/management/articles/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id").value(articleId))
+				// JSON 응답에서 "title" 필드의 값이 'articleDto'의 제목과 일치하는지를 확인
                 .andExpect(jsonPath("$.title").value(articleDto.title()))
                 .andExpect(jsonPath("$.content").value(articleDto.content()))
                 .andExpect(jsonPath("$.userAccount.nickname").value(articleDto.userAccount().nickname()));
+				// JSON 응답에서 "userAccount.nickname" 필드의 값이 "articleDto"의 작성자 닉네임과 일치하는지를 확인
         then(articleManagementService).should().getArticle(articleId);
     }
 
@@ -73,10 +85,12 @@ class ArticleManagementControllerTest {
         // Given
         Long articleId = 1L;
         willDoNothing().given(articleManagementService).deleteArticle(articleId);
+		// 'WillDoNothing()' 메서드를 사용하여 'articleManagementservice'의 'deleteArticle()' 메서드가 호출되었을 때 아무런 동작을 하지 않도록 설정
+
         // When & Then
         mvc.perform(
                         post("/management/articles/" + articleId)
-                                .with(csrf())
+                                .with(csrf())//CSRF공격을 방지하기 위해 웹 애플리케이션에서 사용되는 일반적인 보안 메커니즘
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/management/articles"))
