f38834030a949262a9513040dcb7fa011b798419
asdg2795 <102370423+asdg2795@users.noreply.github.com>
2024-03-14 10:42:13 +0900

    RoleTypesConverter.java 설명 추가

--- src/main/java/com/fastcampus/projectboardadmin/domain/converter/RoleTypesConverter.java
+++ src/main/java/com/fastcampus/projectboardadmin/domain/converter/RoleTypesConverter.java
@@ -18,9 +18,11 @@ public class RoleTypesConverter implements AttributeConverter<Set<RoleType>, Str
     public String convertToDatabaseColumn(Set<RoleType> attribute) {
         return attribute.stream().map(RoleType::name).sorted().collect(Collectors.joining(DELIMETER));
     }
+    // 메서드는 입력으로 받은 역할 집합을 데이터베이스에서 저장될 수 있는 형식의 문자열로 변환하여 반환
 
     @Override
     public Set<RoleType> convertToEntityAttribute(String dbData) {
         return Arrays.stream(dbData.split(DELIMETER)).map(RoleType::valueOf).collect(Collectors.toUnmodifiableSet());
     }
+ 	// 데이터베이스에서 가져온 문자열을 역할 집합으로 변환하여 반환
 }
